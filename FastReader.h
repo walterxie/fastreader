63b3102e44d06c55ff756ccd0d19bbd18b6fa8b2
Walter <walterxie@users.noreply.github.com>
2016-06-15 16:17:04 +1200

    separate interfaces(.h) and Implementations(.cpp)

---
+++ FastReader.h
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+
+// for mmap:
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unordered_map>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class FastReader {
+	public:
+		uintmax_t n_line = 0; // exclude 1st line
+		uintmax_t read_file( const char* fname );
+		void write_file( string outfname );
+		string getFName(void);
+	protected:
+		const char* fname; // input file name and path
+		struct LineStat {
+			int min_pos;
+			int max_pos;
+		};
+		unordered_map <string, LineStat> line_stat_map;		
+		const char* map_file( const char* fname, size_t& length );
+		void assign_line_stat_map( const char* start, const char* end );		
+		void output( ofstream & myfile );
+	private:
+		void handle_error( const char* msg ); 
+};
+
